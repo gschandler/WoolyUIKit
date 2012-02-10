672610e901ac893745b28fe15f2392158cdf5f56
gsc <gsc@woolybeastsoftware.com>
2012-02-10 13:16:08 -0800

    Additional table view categories. This one gets us the last row index path of a section.

---
+++ UITableView+Extensions.h
@@ -0,0 +1,14 @@
+//
+//  UITableView+Extensions.h
+//  WoolyUI
+//
+//  Created by Scott Chandler on 10/18/11.
+//  Copyright (c) 2011 Wooly Beast Software, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITableView (WoolyBeast)
+- (NSIndexPath *)indexPathForLastRowInSection:(NSUInteger)section;
+
+@end
---
+++ UITableView+Extensions.m
@@ -0,0 +1,21 @@
+//
+//  UITableView+Extensions.m
+//  WoolyUI
+//
+//  Created by Scott Chandler on 10/18/11.
+//  Copyright (c) 2011 Wooly Beast Software, LLC. All rights reserved.
+//
+
+#import "UITableView+Extensions.h"
+
+@implementation UITableView (WoolyBeast)
+- (NSIndexPath *)indexPathForLastRowInSection:(NSUInteger)section
+{
+	NSIndexPath *indexPath = nil;
+	if ( self.numberOfSections > section ) {
+		indexPath = [NSIndexPath indexPathForRow:[self numberOfRowsInSection:section]-1 inSection:section];
+	}
+	return indexPath;
+}
+
+@end
