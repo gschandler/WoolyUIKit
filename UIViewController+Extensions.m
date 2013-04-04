9142f068328e6f8ad554d13a1a43e45006724e53
Scott Chandler <gsc@woolybeastsoftware.com>
2013-04-04 14:59:43 -0700

    Fixed warning

--- UIViewController+Extensions.m
+++ UIViewController+Extensions.m
@@ -12,11 +12,12 @@
 
 - (BOOL)isModal
 {
-	BOOL modal = NO;
+	BOOL modal = self.presentingViewController != self;
+#if 0
 	if ( self.parentViewController ) {
 		modal = self.parentViewController.modalViewController == self;
 	}
-	
+#endif
 	return modal;
 }
 @end
