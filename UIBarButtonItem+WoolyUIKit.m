88357bd55f492aaebc2b28f4ce69b0daf8f10918
Scott Chandler <gsc@woolybeastsoftware.com>
2013-06-19 14:57:50 -0700

    MIT copyright license

--- UIBarButtonItem+WoolyUIKit.m
+++ UIBarButtonItem+WoolyUIKit.m
@@ -1,9 +1,27 @@
 //
 //  UIBarButtonItem+WoolyUIKit.m
-//  WoolyUI
 //
-//  Created by Scott Chandler on 1/22/12.
-//  Copyright (c) 2012 Wooly Beast Software, LLC. All rights reserved.
+//	The MIT License (MIT)
+//
+//	Copyright (c) 2013 Wooly Beast Software
+//
+//	Permission is hereby granted, free of charge, to any person obtaining a copy
+//	of this software and associated documentation files (the "Software"), to deal
+//	in the Software without restriction, including without limitation the rights
+//	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//	copies of the Software, and to permit persons to whom the Software is
+//	furnished to do so, subject to the following conditions:
+//
+//	The above copyright notice and this permission notice shall be included in
+//	all copies or substantial portions of the Software.
+//
+//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//	THE SOFTWARE.
 //
 
 #import "UIBarButtonItem+WoolyUIKit.h"
@@ -16,7 +34,11 @@
  */
 + (UIBarButtonItem *)flexibleSpace
 {
-	return [[[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil] autorelease];
+	UIBarButtonItem *barButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
+#if !__has_feature(objc_arc)
+	[barButtonItem autorelease];
+#endif
+	return barButtonItem;
 }
 
 /*
@@ -26,7 +48,10 @@
  */
 + (UIBarButtonItem *)fixedSpaceWithWidth:(CGFloat)width
 {
-	UIBarButtonItem *item = [[[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFixedSpace target:nil action:nil] autorelease];
+	UIBarButtonItem *item = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFixedSpace target:nil action:nil];
+#if !__has_feature(objc_arc)
+	[item autorelease];
+#endif
 	item.width = width;
 	return item;
 }
@@ -38,7 +63,11 @@
  */
 + (UIBarButtonItem *)actionBarButtonItemWithTarget:(id)target action:(SEL)action
 {
-	return [[[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAction target:target action:action] autorelease];
+	UIBarButtonItem *barButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAction target:target action:action];
+#if !__has_feature(objc_arc)
+	[barButtonItem autorelease];
+#endif
+	return barButtonItem;
 }
 
 /*
@@ -48,7 +77,11 @@
  */
 + (UIBarButtonItem *)addBarButtonItemWithTarget:(id)target action:(SEL)action
 {
-	return [[[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:target action:action] autorelease];
+	UIBarButtonItem *barButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:target action:action];
+#if !__has_feature(objc_arc)
+	[barButtonItem autorelease];
+#endif
+	return barButtonItem;
 }
 
 /*
@@ -58,7 +91,11 @@
  */
 + (UIBarButtonItem *)doneBarButtonItemWithTarget:(id)target action:(SEL)action
 {
-	return [[[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:target action:action] autorelease];
+	UIBarButtonItem *barButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:target action:action];
+#if !__has_feature(objc_arc)
+	[barButtonItem autorelease];
+#endif
+	return barButtonItem;
 }
 
 /*
@@ -68,7 +105,11 @@
  */
 + (UIBarButtonItem *)cancelBarButtonItemWithTarget:(id)target action:(SEL)action
 {
-	return [[[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCancel target:target action:action] autorelease];
+	UIBarButtonItem *barButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCancel target:target action:action];
+#if !__has_feature(objc_arc)
+	[barButtonItem autorelease];
+#endif
+	return barButtonItem;
 }
 
 @end
--- UITextField+WoolyUIKit.m
+++ UITextField+WoolyUIKit.m
@@ -1,9 +1,27 @@
 //
 //  UITextField+WoolyUIKit.m
-//  WoolyUI
 //
-//  Created by Scott Chandler on 11/24/10.
-//  Copyright 2010 Wooly Beast Software, LLC. All rights reserved.
+//	The MIT License (MIT)
+//
+//	Copyright (c) 2013 Wooly Beast Software
+//
+//	Permission is hereby granted, free of charge, to any person obtaining a copy
+//	of this software and associated documentation files (the "Software"), to deal
+//	in the Software without restriction, including without limitation the rights
+//	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//	copies of the Software, and to permit persons to whom the Software is
+//	furnished to do so, subject to the following conditions:
+//
+//	The above copyright notice and this permission notice shall be included in
+//	all copies or substantial portions of the Software.
+//
+//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//	THE SOFTWARE.
 //
 
 #import "UITextField+WoolyUIKit.h"
@@ -26,7 +44,10 @@ static NSString * const kUnicodeCrossMark = @"\u274C";
 	if ( show && self.text.length > 0 ) {
 		CGRect frame = self.bounds;
 		frame.size.height = frame.size.width = roundf(frame.size.height * (2.0/3.0));
-		UILabel *label = [[[UILabel alloc] initWithFrame:frame] autorelease];
+		UILabel *label = [[UILabel alloc] initWithFrame:frame];
+#if !__has_feature(objc_arc)
+		[label autorelease];
+#endif
 		label.backgroundColor = [UIColor clearColor];
 		label.font = [UIFont systemFontOfSize:24.0];
 		label.text = kUnicodeCrossMark;
